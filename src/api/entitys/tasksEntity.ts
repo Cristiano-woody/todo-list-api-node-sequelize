333bbef686b8fdddae4af08cbfd4453afdee57d0
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-05-02 09:47:15 -0300

    feat: adding tasksEntity

---
+++ src/api/entitys/tasksEntity.ts
@@ -0,0 +1,29 @@
+import DataTypes from 'sequelize'
+import PostgresDataSource from '../../db/PostgresDataSource'
+import type tasksInterface from '../interfaces/tasksInterface'
+
+export const tasksEntity = PostgresDataSource.define<tasksInterface>('tasks', {
+  id: {
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    autoIncrement: true
+  },
+  done: {
+    type: DataTypes.STRING
+  },
+  date_criation: {
+    type: DataTypes.STRING
+  },
+  description: {
+    type: DataTypes.STRING
+  },
+  title: {
+    type: DataTypes.STRING
+  }
+
+}, {
+  tableName: 'tasks',
+  timestamps: false
+})
+
+export default tasksEntity
