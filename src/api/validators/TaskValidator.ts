10012dba02004821bde189a47d5e44276dfdc63d
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-05-06 14:01:07 -0300

    refactor: adding validate of get task by id

--- src/api/validators/TaskValidator.ts
+++ src/api/validators/TaskValidator.ts
@@ -1,14 +1,23 @@
+import { type Request } from 'express'
 
 class TaskValidator {
-  async CreateTaskValidator (req: { descriptiom: string, done: boolean, title: string }): Promise<boolean> {
+  async CreateTaskValidator (body: { descriptiom: string, done: boolean, title: string }): Promise<boolean> {
     //
-    if (req === null || req === undefined || typeof req !== 'object') {
+    if (body === null || body === undefined || typeof body !== 'object') {
       return false
     }
-    if (req.title === null || req.title === undefined) {
+    if (body.title === null || body.title === undefined) {
       return false
     }
-    if (req.done === null || req.done === undefined) {
+    if (body.done === null || body.done === undefined) {
+      return false
+    }
+    return true
+  }
+
+  async GetTaskByIDValidator (req: Request): Promise<boolean> {
+    //
+    if (req.query.id === null || req.query.id === undefined) {
       return false
     }
     return true
