3afcaba272d9a50c297127987285acff7decae43
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-05-09 17:05:58 -0300

    refactor: renamig the method name in UserRoutes

--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -6,13 +6,13 @@ const userController = new UserController()
 
 router.post('/user', (req: Request, res: Response) => { void userController.CreateUser(req, res) })
 
-router.get('/user', (req: Request, res: Response) => { void userController.getAll(req, res) })
+router.get('/user', (req: Request, res: Response) => { void userController.GetUserById(req, res) })
 
-router.get('/user/:id', (req: Request, res: Response) => { void userController.getById(req, res) })
+router.get('/user/:id', (req: Request, res: Response) => { void userController.GetUserById(req, res) })
 
-router.patch('/user/:id', (req: Request, res: Response) => { void userController.updateByID(req, res) })
+router.patch('/user/:id', (req: Request, res: Response) => { void userController.UpdateUserById(req, res) })
 
-router.delete('/user/:id', (req: Request, res: Response) => { void userController.deleteByID(req, res) })
+router.delete('/user/:id', (req: Request, res: Response) => { void userController.DeleteUserById(req, res) })
 
 //
 //
